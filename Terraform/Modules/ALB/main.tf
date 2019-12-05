c7e8a7f9cd522b7a8dac7f46b3f402b5da67059a
Sephan Bartkowiak <stephan@educat.com.br>
2019-12-05 15:02:03 -0300

    Remove ALB S3 Log

--- Terraform/Modules/ALB/main.tf
+++ Terraform/Modules/ALB/main.tf
@@ -2,12 +2,6 @@
     Resources
 */
 
-resource "aws_s3_bucket" "alb_s3_bucket_log" {
-  bucket = "${var.project_name}-${var.environment}-alb-log"
-  acl    = "private"
-  force_destroy = true
-}
-
 resource "aws_lb" "alb_kubernetes" {
   name               = "ALB-Kubernetes"
   internal           = false
@@ -16,12 +10,6 @@ resource "aws_lb" "alb_kubernetes" {
   subnets            = var.subnets
 
   enable_deletion_protection = true
-
-  access_logs {
-    bucket  = "${var.project_name}-${var.environment}-alb-log"
-    prefix  = "ALB-Log"
-    enabled = true
-  }
 
   tags = var.tag
 }
