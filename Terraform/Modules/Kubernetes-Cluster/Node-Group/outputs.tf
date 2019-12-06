288ca943df1e28927958e166d623b11325294b4f
Stephan Bartkowiak <stephan@ecobonuz.com>
2019-12-05 22:49:30 -0300

    Correct NodeGroup Outputs

--- Terraform/Modules/Kubernetes-Cluster/Node-Group/outputs.tf
+++ Terraform/Modules/Kubernetes-Cluster/Node-Group/outputs.tf
@@ -3,6 +3,6 @@
 */
 
 output "eks_node_group_autoscalling" {
-  value       = aws_eks_node_group.eks_node_group.resources.autoscaling_groups
+  value       = aws_eks_node_group.eks_node_group.resources[0].autoscaling_groups
   description = "EKS NodeGroup - Auto Scalling"
 }
